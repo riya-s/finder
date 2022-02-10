651fce3106758001e95a319af89ece197f73b35b
riya-s <91563940+riya-s@users.noreply.github.com>
2022-02-10 01:59:09 -0600

    Updated Front end

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
 import logo from './logo.svg';
-import './App.css';
+//import './App.css';
+import { useState } from 'react';
+import "./index.css";
+import Title from './Title';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+  <div class="form-container">
+    
+      <form class="register-form">
+
+      <Title />
+        
+        <input
+          id="question"
+          class="form-field"
+          type="text"
+          placeholder="Question"
+          name="question"
+        />
+        
+        <button class="form-field" type="submit">
+          Option - 1
+        </button>
+
+        <button class="form-field" type="submit">
+          Option - 2 
+        </button>
+
+        <button class="form-field" type="submit">
+          Option - 3
+        </button>
+
+        <button class="form-field" type="submit">
+          Option - 4
+        </button>
+
+
+
+      </form>
     </div>
   );
 }
---
+++ src/Title.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Title() {
+  return (
+    <div
+    style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
+      }}>
+
+      <h1>Finder</h1>
+    </div>
+  )
+}
+
+export default Title
